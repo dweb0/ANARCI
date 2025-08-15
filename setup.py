554982769ae153e609d8740edc300966d73cbea6
Daniel Weber <dgw@posteo.net>
2025-08-14 23:32:22 -0700

    fixed issue that was requiring 2x install

--- setup.py
+++ setup.py
@@ -1,46 +1,20 @@
 #!/usr/bin/env python3
 
+
 #                               #
 # Developed by James Dunbar     #
 # Maintained by members of OPIG #
 #                               #
 
-import shutil, os, subprocess, importlib
+import site
+import shutil, os
 # Clean this out if it exists
 if os.path.isdir("build"):
     shutil.rmtree("build/")
 
-from distutils.core import setup
-
-setup(name='anarci',
-      version='1.3',
-      description='Antibody Numbering and Receptor ClassIfication',
-      author='James Dunbar',
-      author_email='opig@stats.ox.ac.uk',
-      url='http://opig.stats.ox.ac.uk/webapps/ANARCI',
-      packages=['anarci'], 
-      package_dir={'anarci': 'lib/python/anarci'},
-      #package_data={'anarci': ['dat/HMMs/ALL.hmm',
-      #                         'dat/HMMs/ALL.hmm.h3f',
-      #                         'dat/HMMs/ALL.hmm.h3i',
-      #                         'dat/HMMs/ALL.hmm.h3m',
-      #                         'dat/HMMs/ALL.hmm.h3p']},
-      scripts=['bin/ANARCI'],
-      data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS']) ]
-     )
-
-####
-import sys
-if sys.argv[1] != "install":
-    sys.exit(0)
-
-try:
-    ANARCI_LOC = importlib.util.find_spec("anarci").submodule_search_locations[0]
-except Exception as e:
-    sys.stderr.write(e.message)
-    sys.exit(1)
-
-os.chdir("build_pipeline")
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.build_py import build_py as _build_py
 
 # The main change in this fork is that I have run the build pipeline once,
 # and will use those results for all future builds. Since I'm building
@@ -48,6 +22,43 @@ os.chdir("build_pipeline")
 # or their API is down, which has happened to me before without
 # realizing.
 # Built on 2022-11-29, IMGT release 202246-1 (14 November 2022) 
+
+class build_py(_build_py):
+    def run(self):
+        anarci_dir = os.path.join("lib", "python", "anarci")
+
+        germ_src = os.path.join("build_pipeline", "curated_alignments", "germlines.py")
+        germ_dst = os.path.join(anarci_dir, "germlines.py")
+        shutil.copy2(germ_src, germ_dst)
+
+        hmm_src = os.path.join("build_pipeline", "HMMs")
+        hmm_dst = os.path.join(anarci_dir, "dat", "HMMs")
+        os.makedirs(os.path.dirname(hmm_dst), exist_ok=True)
+        shutil.copytree(hmm_src, hmm_dst, dirs_exist_ok=True)
+
+        _build_py.run(self)
+
+
+setup(
+    name='anarci',
+    version='1.3',
+    description='Antibody Numbering and Receptor ClassIfication',
+    author='James Dunbar',
+    author_email='opig@stats.ox.ac.uk',
+    url='http://opig.stats.ox.ac.uk/webapps/ANARCI',
+    packages=find_packages('lib/python'),
+    package_dir={'': 'lib/python'},
+    include_package_data=True,
+    package_data={
+        "anarci": [
+            "germlines.py",
+            "dat/HMMs/*"
+        ]
+    },
+    scripts=['bin/ANARCI'],
+    data_files=[('bin', ['bin/muscle', 'bin/muscle_macOS'])],
+    cmdclass={ 'build_py': build_py }
+)
 
 # try:
 #     #shutil.rmtree("curated_alignments/")
@@ -64,7 +75,3 @@ os.chdir("build_pipeline")
 
 # print(o.decode())
 # print(e.decode())
-
-
-shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
-shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
